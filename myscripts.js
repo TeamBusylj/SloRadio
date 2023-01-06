472dd6864c6940f562efa4321259074bb4fed0ff
TeamBusylj <88550318+TeamBusylj@users.noreply.github.com>
2023-01-06 15:58:28 +0100

    x

--- myscripts.js
+++ myscripts.js
@@ -5,11 +5,7 @@ function getFavi(y, x, z) {
    
 }
 
-if ('addEventListener' in document) {
-	document.addEventListener('DOMContentLoaded', function() {
-		FastClick.attach(document.body);
-	}, false);
-}
+
 function favi(y, x, z) {
     localStorage.setItem(z, x + y + z); 
     console.log(localStorage)

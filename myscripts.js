0ee0fb100e6f05d9e080a477d9b2c2c5d65ab5c7
TeamBusylj <88550318+TeamBusylj@users.noreply.github.com>
2023-01-07 09:52:42 +0100

    o

--- myscripts.js
+++ myscripts.js
@@ -108,13 +108,7 @@ function myShow() {
     element.classList.remove("screen");
     let elem = document.querySelector("body");
 
-    if (!document.fullscreenElement) {
-      elem.requestFullscreen().catch((err) => {
-        alert(`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`);
-      });
-    } else {
-      document.exitFullscreen();
-    }
+   showToast()
 }
 function loads(){
     

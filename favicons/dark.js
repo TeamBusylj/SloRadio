9c9b2249c4fad96054584c8eb9454aa2c5db84f9
TeamBusylj <88550318+TeamBusylj@users.noreply.github.com>
2023-02-05 21:52:15 +0100

    hjk

--- favicons/dark.js
+++
@@ -1,7 +0,0 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  var color = '#071636';
-  document.getElementById("colorka").innerHTML=color;
-}else{
-    var color = '#57b3ff';
-    document.getElementById("colorka").innerHTML=color;
-}
